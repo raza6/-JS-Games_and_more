1b64647a4ac261e36715ce68525bd2d619eb9b47
raza6 <gontier.antonin@orange.fr>
2016-11-19 00:47:18 +0100

    Update

--- Magic/magic.js
+++ Magic/magic.js
@@ -2,7 +2,7 @@ var alea;
 
 alea = Math.floor(Math.random()*(1000)+1);
 
-/* console.log('Le chiffre magic est ' + alea); */ 
+console.log('Le chiffre magic est ' + alea);
 
 for (var i = 0, user; i < 10; i++) {
 
@@ -12,14 +12,22 @@ user = prompt('Saisir un chiffre entre 1 et 1000');
 		alert('Gagné !');
 		break;
 	} else if (user < 1000 && user > 0 && alea > user) {
-		alert('Perdu ! ' + 'Chiffre magic > ' + user);
+		if (alea-user <= 10) {
+			alert('Presque ! ' + 'Le chiffre magic est légérement supérieur à ' + user);
+		} else {
+			alert('Perdu ! ' + 'Le chiffre magic est supérieur à ' + user);
+		}
 	} else if (user < 1000 && user > 0 && alea < user) {
-		alert('Perdu ! ' + 'Chiffre magic < ' + user);
+		if (user-alea <= 10) {
+			alert('Presque ! ' + 'Le chiffre magic est légérement inférieur à ' + user);
+		} else {
+			alert('Perdu ! ' + 'Le chiffre magic est inférieur à ' + user);
+		}
 	} else {
 		alert('What ?!')
 	}
 }
 
 if (alea != user) {
-	alert ('Perdu, vous n\'avez plus de tentative supplémentaire ! Le chiffre magik était ' + alea + '.')
+	alert ('Perdu, vous n\'avez plus de tentative supplémentaire ! Le chiffre magic était ' + alea + '.')
 }
